232aa147fe414aab425c6236f45bec9765dac1b6
milleide <milleide_catarina@yahoo.com.br>
2020-12-06 18:55:33 -0300

    condição para busca e outras funcionalidades

--- src/br/edu/prjagenda2/Principal.java
+++ src/br/edu/prjagenda2/Principal.java
@@ -25,7 +25,7 @@ public class Principal {
             op = Integer.parseInt(sc.nextLine());
             switch (op) {
                 case 1:
-                    principal.Cadastrar();
+                    principal.CadastrarPJ();
                     break;
                 case 2:
                     principal.CadastrarPF();
@@ -48,80 +48,105 @@ public class Principal {
         } while (op != 6);
 
     }
-    public void Cadastrar() {
-            ContatoPJ cont = new ContatoPJ();
+    public void CadastrarPJ() {
+            ContatoPJ contatoPJ = new ContatoPJ();
             Scanner sc = new Scanner(System.in);
-            System.out.println("Escreva o nome do contato");
-            cont.setNome(sc.nextLine());
-            System.out.println("Escreva o email do contato");
-            cont.setEmail(sc.nextLine());
-            System.out.println("Escreva o telefone");
-        try {
-            cont.setTelefone(Integer.parseInt(sc.nextLine()));
-        } catch (NumberFormatException e) {
-            System.out.println("Telefone inválido!");
-        }
-        System.out.println("Escreva o site da empresa");
-            cont.setHomepage(sc.nextLine());
-            System.out.println("Escreva o funcionário");
-            cont.setFuncionário(sc.nextLine());
-            this.contatos.add(cont);
+            System.out.println("\nDigite:\n");
+            System.out.println("nome da empresa: ");
+            contatoPJ.setNome(sc.nextLine());
+            System.out.println("email: ");
+            contatoPJ.setEmail(sc.nextLine());
+        int t = 0;
+        do{
+            System.out.println("telefone: ");
+            try {
+                contatoPJ.setTelefone(Integer.parseInt(sc.nextLine()));
+                t=1;
+            } catch (NumberFormatException e) {
+                System.out.println("Telefone inválido!");
+            }
+        } while(t != 1);
+        System.out.println("site: ");
+            contatoPJ.setHomepage(sc.nextLine());
+            System.out.println("nome do contato na empresa: ");
+            contatoPJ.setFuncionário(sc.nextLine());
+            this.contatos.add(contatoPJ);
         System.out.println("\nContato PJ adicionado com sucesso!");
         };
 
     public void CadastrarPF(){
         ContatoPF contatoPF = new ContatoPF();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Escreva o nome do contato");
+        System.out.println("\nDigite:\n");
+        System.out.println("nome do contato: ");
         contatoPF.setNome(sc.nextLine());
-        System.out.println("Escreva o email do contato");
+        System.out.println("email: ");
         contatoPF.setEmail(sc.nextLine());
-        System.out.println("Escreva o telefone");
-        try {
-            contatoPF.setTelefone(Integer.parseInt(sc.nextLine()));
-        } catch (NumberFormatException e) {
-            System.out.println("Telefone inválido!");
-        }
-        System.out.println("Digite a data de nascimento: ");
-        try {
-            contatoPF.setDataNascimento(sc.nextLine());
-        } catch (ParseException e) {
-            System.out.println("Data inválida!");;
-        }
+        int t = 0;
+        do{
+            System.out.println("telefone: ");
+            try {
+                contatoPF.setTelefone(Integer.parseInt(sc.nextLine()));
+                t=1;
+            } catch (NumberFormatException e) {
+                System.out.println("Telefone inválido!");
+            }
+        } while(t != 1);
+        do{
+            System.out.println("data de nascimento: ");
+            try {
+                contatoPF.setDataNascimento(sc.nextLine());
+                t=2;
+            } catch (ParseException e) {
+                System.out.println("Data inválida!");
+            }
+        } while(t != 2);
         this.contatos.add(contatoPF);
         System.out.println("\nContato PF adicionado com sucesso!");
     }
 
+
     public void Listar(){
+        if (contatos.isEmpty()){
+            System.out.println("\nAgenda Vazia!");
+        }else {
         for (ContatoBasico c : this.contatos){
-            if (c != null) {
-                System.out.println(c.toString());
+            {
+            System.out.println(c.toString());
+            }
             }
         }
     }
 
     public void Buscar() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Escreva o nome para busca");
+        System.out.println("Digite o nome para busca: ");
         String nome = sc.nextLine();
         for (int i = 0; i < contatos.size(); i++){
-            if(contatos.get(i).getNome().equals(nome)){
+            if(contatos.get(i).getNome().equalsIgnoreCase(nome)){
                 System.out.println(contatos.get(i));
+            } else {
+                System.out.println("\nContato não encontrado!");
             }
         }
 
     }
     public void Excluir() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Escreva o nome do contato para excluir");
+        System.out.println("Digite o nome do contato para exclusão: ");
         String nome = sc.nextLine();
         for (int i = 0; i < contatos.size(); i++){
-            if(contatos.get(i).getNome().equals(nome)){
+            if(contatos.get(i).getNome().equalsIgnoreCase(nome)){
             contatos.remove(i);
+                System.out.println("\nContato excluído com sucesso!");
+            } else {
+                System.out.println("\nContato não encontrado!");
             }
         }
 
     }
+
+
 }
 
 
